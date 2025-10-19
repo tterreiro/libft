54cfd7d32c8b3d51831af58671e1147b065577e0
terreiro <jaruvouteaocu@gmail.com>
2025-10-19 19:04:32 +0100

    added some str based functions

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,50 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-andr <hde-andr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/17 16:13:39 by hde-andr          #+#    #+#             */
-/*   Updated: 2025/10/17 16:14:11 by hde-andr         ###   ########.fr       */
+/*   Created: 2025/10/19 16:55:49 by hde-andr          #+#    #+#             */
+/*   Updated: 2025/10/19 16:55:49 by hde-andr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*tmp;
-	size_t	size;
-	size_t	i;
+	unsigned char	*tmp;
+	size_t			s1_len;
+	size_t			s2_len;
+	size_t			i;
 
-	if (s == NULL || f == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	size = ft_strlen(s);
-	tmp = malloc(sizeof(char) * (size + 1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	tmp = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (tmp == NULL)
 		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		tmp[i] = f(i, s[i]);
-		i++;
-	}
-	tmp[i] = '\0';
+	ft_memcpy(tmp, s1, s1_len);
+	ft_memcpy(tmp + s1_len, s2, s2_len);
+	tmp[s1_len + s2_len] = '\0';
 	return (tmp);
 }
 
 /*#include <stdio.h>
-char plus_one(unsigned int i, char c)
+int	main()
 {
-	return c + 1;
-}
-
-int main(void)
-{
-	const char *str = "012345678";
-	char *result = ft_strmapi(str, plus_one);
-	printf("%s\n", result); 
-	free(result);
-	return 0;
+	char const	*s1 = "Hello ";
+	char const	*s2 = "World!";
+	printf("%s\n", ft_strjoin(s1, s2));
 }*/
