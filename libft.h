12d48113d19833c4d9ee51b7d3b207dccd40bf61
terreiro <jaruvouteaocu@gmail.com>
2025-10-20 20:56:13 +0100

    split is finally done omg

--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
